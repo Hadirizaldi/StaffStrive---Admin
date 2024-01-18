d00328674b933c2c18cdd5e6dee5620c7fe2d340
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2024-01-18 22:37:52 +0700

    scaffolding admin template

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,35 @@
+<aside id="left-panel" class="left-panel">
+    <nav class="navbar navbar-expand-sm navbar-default">
+        <div id="main-menu" class="main-menu collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="index.html"><i class
+                        ="menu-icon fa fa-laptop"></i>Dashboard </a>
+                </li>
+                <li class="menu-title">Karyawan</li><!-- /.menu-title -->
+                <li class="">
+                    <a href="#"> <i class="menu-icon fa fa-list"></i>Lihat Karyawan</a>
+                </li>
+                <li class="">
+                    <a href="#"> <i class="menu-icon fa fa-plus"></i>Tambah Karyawan</a>
+                </li>
+
+                <li class="menu-title">Departemen</li><!-- /.menu-title -->
+                <li class="">
+                    <a href="#"> <i class="menu-icon fa fa-list"></i>Lihat Departemen</a>
+                </li>
+                <li class="">
+                    <a href="#"> <i class="menu-icon fa fa-plus"></i>Tambah Departemen</a>
+                </li>
+
+                <li class="menu-title">Posisi</li><!-- /.menu-title -->
+                <li class="">
+                    <a href="#"> <i class="menu-icon fa fa-list"></i>Lihat Posisi</a>
+                </li>
+                <li class="">
+                    <a href="#"> <i class="menu-icon fa fa-list"></i>Tambah Posisi</a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</aside>
