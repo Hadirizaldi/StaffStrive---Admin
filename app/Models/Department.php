737d63d41bbfd95fcf8e8f43ed24a8047e56cafc
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2024-01-18 23:33:46 +0700

    create connections between tables through models

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Department extends Model
@@ -17,4 +18,9 @@ class Department extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function positon(): HasMany
+    {
+        return $this->hasMany(Position::class, 'department_id', 'id');
+    }
 }
--- app/Models/Position.php
+++ app/Models/Position.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Position extends Model
@@ -18,4 +20,16 @@ class Position extends Model
         'name',
         'department_id'
     ];
+
+    // relation with department model
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class, 'department_id', 'id');
+    }
+
+    // relation with employee model
+    public function employee(): HasMany
+    {
+        return $this->hasMany(Employee::class, 'position_id', 'id');
+    }
 }
